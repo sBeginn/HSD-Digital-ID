e7939d40b2c3e5c32cc6109c55a3e3b759ed4c15
sBeginn <steffen.beginn@study.hs-duesseldorf.de>
2023-02-10 19:33:52 +0100

    added test_database function

---
+++ hsd_digital_id/tests/test_database.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def test_database():
+        # Connect to database
+        connection = sqlite3.connect("students.db")
+        cursor = connection.cursor()
+
+        # Select a column from the database
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='students';")
+        result = cursor.fetchone()
+        
+        # Checks if the database was created
+        assert result is not None, "Database not created"
+        
+        # Close and disconnect the database
+        connection.close()
